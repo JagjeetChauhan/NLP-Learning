9befa32725350bc2b6fd22f8ff78a18e119c6558
JagjeetSinghChauhan <98844703+JagjeetChauhan@users.noreply.github.com>
2026-06-06 14:05:22 +0000

    Lecture 2: Building the Initial Byte Vocabulary. Word to byte tokens

--- tokenizers-from-scratch/Byte-Level BPE.py
+++ tokenizers-from-scratch/Byte-Level BPE.py
@@ -20,8 +20,65 @@ def text_to_bytes(text):
         text.encode("utf-8")
     )
 
+# =========================================================
+# BYTES TO TEXT
+# =========================================================
+#
+# Converts byte values back into text.
+#
+# Example:
+#
+# [104,101,108,108,111]
+#
+# ->
+#
+# hello
+#
+# =========================================================
+
+def bytes_to_text(byte_values):
+
+    return bytes(
+        byte_values
+    ).decode(
+        "utf-8"
+    )
+
+# =========================================================
+# WORD TO BYTE TOKENS
+# =========================================================
+#
+# Converts word into UTF-8 byte tokens.
+#
+# Example:
+#
+# low
+#
+# ->
+#
+# ['108','111','119']
+#
+# =========================================================
+
+def word_to_byte_tokens(word):
+
+    byte_values = list(
+        word.encode("utf-8")
+    )
+
+    byte_tokens = [
+
+        str(byte)
+
+        for byte in byte_values
+    ]
+
+    return byte_tokens
 
 #=======================================Testing======================================================
 text = "Hello"
 print("Text: ",text)
-print("\nText to Byte:",text_to_bytes(text))
+byte = text_to_bytes(text)
+print("\nText to Byte:",byte)
+
+print("\nByte to Text: ",bytes_to_text(byte))

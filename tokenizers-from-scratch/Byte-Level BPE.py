0d8e57ca6b5a7ad8329389bcde76428fd677513e
JagjeetSinghChauhan <98844703+JagjeetChauhan@users.noreply.github.com>
2026-06-06 14:07:18 +0000

    Lecture 3: Pair Counting on Bytes

--- tokenizers-from-scratch/Byte-Level BPE.py
+++ tokenizers-from-scratch/Byte-Level BPE.py
@@ -75,10 +75,109 @@ def word_to_byte_tokens(word):
 
     return byte_tokens
 
-#=======================================Testing======================================================
-text = "Hello"
-print("Text: ",text)
-byte = text_to_bytes(text)
-print("\nText to Byte:",byte)
+# =========================================================
+# BUILD BYTE VOCABULARY
+# =========================================================
+#
+# Converts corpus into byte-level
+# vocabulary representation.
+#
+# =========================================================
 
-print("\nByte to Text: ",bytes_to_text(byte))
+
+def build_byte_vocab(corpus):
+
+    vocab = defaultdict(int)
+
+    for word in corpus:
+
+        tokens = (
+            word_to_byte_tokens(word)
+            +
+            ["</w>"]
+        )
+
+        vocab[
+            tuple(tokens)
+        ] += 1
+
+    return vocab
+
+# =========================================================
+# GET BYTE PAIR COUNTS
+# =========================================================
+#
+# Counts frequency of adjacent
+# byte token pairs.
+#
+# =========================================================
+
+from collections import defaultdict
+
+
+def get_byte_pair_counts(
+    vocab
+):
+
+    pair_counts = defaultdict(
+        int
+    )
+
+    for word, freq in vocab.items():
+
+        for i in range(
+            len(word) - 1
+        ):
+
+            pair = (
+
+                word[i],
+
+                word[i + 1]
+            )
+
+            pair_counts[
+                pair
+            ] += freq
+
+    return pair_counts
+
+#=======================================Testing======================================================
+corpus = [
+
+    "low",
+
+    "lowest",
+
+    "newer"
+]
+
+byte_vocab = build_byte_vocab(
+    corpus
+)
+
+for word, freq in byte_vocab.items():
+
+    print(
+        word,
+        "->",
+        freq
+    )
+
+pair_counts = (
+    get_byte_pair_counts(
+        byte_vocab
+    )
+)
+
+for pair, count in sorted(
+    pair_counts.items(),
+    key=lambda x: x[1],
+    reverse=True
+):
+    print("\nPair Count:")
+    print(
+        pair,
+        "->",
+        count
+    )

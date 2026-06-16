66f82655e82443ab1ab297536d588e0c0f17982b
JagjeetSinghChauhan <98844703+JagjeetChauhan@users.noreply.github.com>
2026-06-16 15:56:41 +0000

    training progress till getting adj pairs

--- tokenizers-from-scratch/WordPiece Tokenizer From Scratch/WordPiece Tokenizer.py
+++ tokenizers-from-scratch/WordPiece Tokenizer From Scratch/WordPiece Tokenizer.py
@@ -11,7 +11,40 @@ def word_split(corpus):
 
 lists = word_split(corpus)
 print(lists)
+print("\n")
 
-for words in lists:
-    for word in words:
-        print(word)
+# Word frequencies
+word_freq = {}
+
+for count_words in lists:
+    for count_word in count_words:
+        if count_word in word_freq:
+            word_freq[count_word] += 1
+        else:
+            word_freq[count_word] = 1
+
+print(word_freq)
+print("\n")
+
+# Split words into WordPiece-style characters
+splits = {}
+
+for word_to_chr in word_freq:
+    splits[word_to_chr] = [word_to_chr[0]] + [f"##{c}" for c in word_to_chr[1:]]
+
+print(splits)
+print("\n")
+
+# Find adjacent pairs
+pairs = []
+
+for word, tokens in splits.items():
+    print(f"\n{word}: {tokens}")
+
+    for i in range(len(tokens) - 1):
+        pair = (tokens[i], tokens[i+1])
+        pairs.append(pair)
+        print(pair)
+
+print("\nAll pairs:")
+print(pairs)

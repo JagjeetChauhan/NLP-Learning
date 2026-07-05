8ac8e7e376b30bf9e1821d976d5387eebb160958
JagjeetSinghChauhan <98844703+JagjeetChauhan@users.noreply.github.com>
2026-07-05 15:02:48 +0000

    updated vocab storage way.

--- tokenizers-from-scratch/SentencePiece from scratch/SentencePiece Tokenizer.py
+++ tokenizers-from-scratch/SentencePiece from scratch/SentencePiece Tokenizer.py
@@ -2,32 +2,33 @@
 Replace spaces with ▁
 Treat each sentence as one sequence """
 
-    def Text_preprocess(sentence):
-        result = ""
-        for line in sentence:
-            for ch in line:
-                if ch ==' ':
-                    result += '_'
-                else:
-                    result+= ch
-            result += '\n'
+def Text_preprocess(sentence):
+    result = ""
+    for line in sentence:
+        for ch in line:
+            if ch ==' ':
+                result += '_'
+            else:
+                result += ch
+        result += '\n'
         
-        return result
+    return result
 
-    sentence = ["Hello I am Luv",
-                "I am studing"]
-    vocab = Text_preprocess(sentence)
-    print(vocab)
+sentence = ["Hello I am Luv",
+           "I am studing"]
+vocab = Text_preprocess(sentence)
+print(vocab)
 
-    def add_to_storage(updated_sentences):
-        vocab_storage = []
-        for i in updated_sentences:
-            vocab_storage.append(i)
-        
-        return vocab_storage
+def Text_preprocess(sentences):
+    result = []
 
-    vocab_storage = add_to_storage(vocab)
-    print(vocab_storage)
+    for sentence in sentences:
+        result.append(sentence.replace(" ", "▁"))
+
+    return result
+
+vocab = Text_preprocess(sentence)
+print(vocab)
 
 """
 Stage 2 — Initial vocabulary

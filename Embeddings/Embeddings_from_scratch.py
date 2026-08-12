98e6fea6886dd40636b1fefad27469f42769aaee
JagjeetSinghChauhan <98844703+JagjeetChauhan@users.noreply.github.com>
2026-08-12 11:48:01 +0000

    Updated Embedding Layer code with class.

--- Embeddings/Embeddings_from_scratch.py
+++ Embeddings/Embeddings_from_scratch.py
@@ -53,3 +53,43 @@ batch_sentences = np.array([[4,5,6],[7,8,9]])
 vector_sentence = embedding_matrix[batch_sentences]
 print("Batch of Sentences")
 print(vector_sentence)
+
+#---------------------------------------------------------Production Level Code------------------------------------------
+def Embedding(vocab_size, embedding_dim, padding_idx=None):
+    embedding_matrix = np.random.normal(loc=0.0, scale=0.01, size=(vocab_size, embedding_dim)).astype(np.float32)
+    if padding_idx is not None:
+        embedding_matrix[padding_idx] = 0.0
+    
+    return embedding_matrix
+
+def forward(token_ids, embedding_matrix):
+    token_ids = np.asarray(token_ids, dtype=np.int64)
+    return embedding_matrix[token_ids]
+
+import numpy as np
+
+embedding = Embedding(
+    vocab_size=10,
+    embedding_dim=4,
+    padding_idx=6
+)
+
+print(embedding.shape)
+
+batch = np.array([
+    [1, 2, 3, 6],   # I love NLP <PAD>
+    [4, 5, 6, 6]    # cats dogs <PAD> <PAD>
+])
+
+output = forward(batch, embedding)
+
+print(output.shape)
+print()
+print(output)
+
+# ( Batch Size, Sequence Length, Embedding Dimension )
+#       ↓            ↓                ↓
+#       2            4                4
+#       ↓            ↓                ↓
+#     No. of     each sentence   Each token in each sentence is represented by a vector containing 4 features.
+#    Sentence  contains 4 tokens
